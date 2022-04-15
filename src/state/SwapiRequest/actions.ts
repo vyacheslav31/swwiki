14b5f9f136573f0164d7e8c19cc12c91747481e3
Vyacheslav Gorbov <vyacheslav93@gmail.com>
2022-04-14 22:19:05 -0400

    removed SWAPI_REQUEST_FINISHED from SwapiRequestType enum

--- src/state/SwapiRequest/actions.ts
+++ src/state/SwapiRequest/actions.ts
@@ -2,7 +2,6 @@ export enum SwapiRequestType {
   SWAPI_REQUEST_LOADING = "swapi_request_loading",
   SWAPI_REQUEST_SUCCESS = "swapi_request_success",
   SWAPI_REQUEST_ERROR = "swapi_request_error",
-  SWAPI_REQUEST_FINISHED = "swapi_request_finished",
 }
 
 export type SwapiResponse = {
